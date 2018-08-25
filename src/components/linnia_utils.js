fb45a90cecb39c2f211c0c97b3edee309b7a25b6
fodisi <fodisi@users.noreply.github.com>
2018-08-25 19:08:38 -0400

    Added event handlers

--- src/components/linnia_utils.js
+++ src/components/linnia_utils.js
@@ -1,7 +1,7 @@
 import config from '../config';
 import Web3 from 'web3';
 import IPFS from 'ipfs-mini';
-import {encrypt} from './crypto-utils';
+import { encrypt } from './crypto-utils';
 import Linnia from '@linniaprotocol/linnia-js';
 import React, { Component } from 'react';
 
@@ -15,19 +15,19 @@ const gas = 500000
 
 
 const web3 = new Web3(window.web3.currentProvider)
-  
+
 const ipfs = new IPFS({ host: host, port: port, protocol: protocol })
 
 const linnia = new Linnia(web3, ipfs, { hubAddress })
 
 
-async function createRecord(metadata, data, ownerPublicKey){
+async function createRecord(metadata, data, ownerPublicKey) {
   let encrypted, ipfsRecord;
-try {
+  try {
     encrypted = await encrypt(new Buffer(ownerPublicKey, 'hex'), data)
   } catch (e) {
     console.error(e)
-	return
+    return
   }
 
   try {
@@ -38,12 +38,12 @@ try {
     })
   } catch (e) {
     console.error(e)
-	return
+    return
   }
 
   const dataUri = ipfsRecord[0].hash
   const [owner] = await web3.eth.getAccounts()
-  const dataHash = await web3.util.soliditySha3(metadata, dataUri) 
+  const dataHash = await web3.util.soliditySha3(metadata, dataUri)
 
   try {
     const { records } = await linnia.getContractInstances()
@@ -61,16 +61,18 @@ try {
 
 export class AddRecord extends Component {
 
-	constructor (props) {
-    	super(props)
-    	this.state = {
-      		event: '',
-      		description: '',
-      		owner_pk:'',
-    	}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      event: '',
+      description: '',
+      owner_pk: '',
+      propety: '',
 
-   handleSubmit = (event) => {
+    }
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault()
     const metadata = event.target.elements.event.value
     const data = event.target.elements.description.value
@@ -78,31 +80,42 @@ export class AddRecord extends Component {
     createRecord(metadata, data, pk)
   }
 
-  render () {
+  onInputChange = (property) => (event) => {
+    const value = event.target.value;
+    this.setState({ [property]: value });
+  }
+
+  render() {
     return (
       <form className='pure-form pure-form-stacked' onSubmit={this.handleSubmit}>
         <fieldset>
-          <label htmlFor='event'>Event Name</label>
-          <input id='event' type='text' value={this.state.event} placeholder='Event Name' />
+          <label htmlFor='event'>{"Event Name"}</label>
+          <input
+            id='event'
+            type='text'
+            onChange={this.onInputChange('event')}
+            value={this.state.event}
+            placeholder='Event Name'
+          />
 
           <br />
 
           <label htmlFor='description'>Event Description</label>
-          <input id='description' type='text' value={this.state.description}  placeholder='Event Details' />
+          <input id='description' type='text' value={this.state.description} onChange={this.onInputChange('description')} placeholder='Event Details' />
 
-          
+
 
           <br />
           <label htmlFor='public_key'>Your Public Key</label>
-          <input id='public_key' type='text' value={this.state.owner_pk} placeholder='Public Key' />
+          <input id='public_key' type='text' value={this.state.owner_pk} onChange={this.onInputChange('owner_pk')} placeholder='Public Key' />
 
-          
+
 
           <br />
 
           <button type='submit' className='pure-button pure-button-primary'>Create Event</button>
         </fieldset>
       </form>
-      );
+    );
   }
 }
