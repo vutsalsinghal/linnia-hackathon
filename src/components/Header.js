849abf3e88a94debb6634508695f9815ceac2fc9
vutsalsinghal <vutsalsinghal@gmail.com>
2018-08-27 22:11:40 -0400

    Added react-router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import { Menu, Modal, Icon } from 'semantic-ui-react';
-import { AddRecord } from './AddRecord';
-//import { AcceptDeclineContainer } from './AcceptDeclineContainer';
 import { AddReferral } from './AddReferral';
 
 export default () => {
     return (
         <div>
             <Menu style={{ marginTop: '10px' }}>
-                <Menu.Item>SecretEvents</Menu.Item>
+                <Menu.Item><Link to='/'>Inner Circle</Link></Menu.Item>
 
                 <Menu.Menu position="right">
-                    <Modal trigger={<Menu.Item><Icon name='add' />Add Event</Menu.Item>}>
-                        <Modal.Header>Create Secret Event</Modal.Header>
-                        <Modal.Content>
-                            <AddRecord />
-                        </Modal.Content>
-                    </Modal>
+                    <Menu.Item><Link to='/addEvent'><Icon name='add' />Add Event</Link></Menu.Item>
                 </Menu.Menu>
                 <Menu.Menu>
                     <Modal trigger={<Menu.Item>Send Referral</Menu.Item>}>
---
+++ src/components/Home.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { Grid, Card, Icon, Modal, Button } from 'semantic-ui-react';
+import { AcceptDeclineContainer } from './AcceptDeclineContainer';
+import SecretEventOrg from '../ethereum/SecretEventOrg';
+import Web3 from 'web3';
+import FullDetail from './FullDetail';
+import { checkIfMember, checkIfOwner, checkIfReferred } from '../actions/ReferralAction';
+
+class Home extends Component {
+  state = {
+    eventName: '',
+    describe: '',
+    capacity: 0,
+    deposit: 0,
+    start_time: 0,
+    duration: 0,
+    isOwner: false,
+    isMember: false,
+    isReferral: false,
+  }
+
+  async componentDidMount() {
+    var eventHash = await SecretEventOrg.methods.currentEventHash().call();
+    let { eventName, describe, capacity, deposit, start_time, duration } = await SecretEventOrg.methods.getEventInfo(eventHash).call();
+    const isOwner = await checkIfOwner();
+    const isMember = await checkIfMember();
+    const isReferral = await checkIfReferred();
+    this.setState({ eventName, describe, capacity, deposit, start_time, duration, isOwner, isMember, isReferral });
+  }
+
+  renderEvent() {
+    return (
+      <Card>
+        <Card.Content>
+          <Card.Header>Name: {this.state.eventName}</Card.Header>
+          <Card.Meta>Capacity: {this.state.capacity}</Card.Meta>
+          <Card.Description>Min Deposit: {Web3.utils.fromWei(this.state.deposit.toString(), 'ether')} ether</Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1></h1>
+        <Grid stackable reversed="mobile">
+          <Grid.Column width={12}>
+            {this.renderEvent()}
+          </Grid.Column>
+          <Grid.Column width={4}>
+            {
+              this.state.isReferral &&
+              <Grid.Row>
+                <Modal size='small'
+                  trigger={
+                    <Button icon labelPosition='left' className="primary" floated="right">
+                      <Icon name='add' />
+                      Pending Referral
+                    </Button>
+                  }>
+                  <Modal.Header>Accept/Decline Referral</Modal.Header>
+                  <Modal.Content>
+                    <AcceptDeclineContainer />
+                  </Modal.Content>
+                </Modal>
+              </Grid.Row>
+            }
+
+            {this.state.isMember &&
+              <Grid.Row>
+                <Modal size='small'
+                  trigger={
+                    <Button icon labelPosition='left' className="primary" floated="right">
+                      <Icon name='add' />
+                      Full Details
+                   </Button>
+                  }>
+                  <Modal.Header>Full Details of Event</Modal.Header>
+                  <Modal.Content>
+                    <FullDetail />
+                  </Modal.Content>
+                </Modal>
+              </Grid.Row>
+            }
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Home;
