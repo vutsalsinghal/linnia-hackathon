9935b6c46b2ef8542815cea28fded6244d51922a
vutsalsinghal <vutsalsinghal@gmail.com>
2018-09-19 12:26:42 -0400

    Updated testnet

--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -29,7 +29,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
 	web3 = new Web3(window.web3.currentProvider);
 }else{
 	// We are on the server *OR* the user is not running metamask
-	const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/fmYYH0aPRKNF5MFSuVNH');
+	const provider = new Web3.providers.HttpProvider('https://ropsten.infura.io/fmYYH0aPRKNF5MFSuVNH');
 	web3 = new Web3(provider);
 }
 
